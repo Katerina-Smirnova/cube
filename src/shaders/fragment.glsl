a438845621b2f80942a2b0a150460a16d7313e2f
Katerina-Smirnova <katerunasmirnova@mail.ru>
2026-07-27 23:59:47 +0300

    шейдер

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,4 +1,18 @@
 precision mediump float;
+uniform float uTime;
+
+varying vec2 vUv;
 void main() {
-    gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+    vec3 color1 = vec3(1.0, 0.0, 0.0); // Красный
+    vec3 color2 = vec3(0.0, 0.0, 1.0); // Синий
+    vec3 color3 = vec3(0.0, 1.0, 0.0); // Зеленый
+
+    float mixFactor = sin(uTime * 0.3) * 0.3 + 0.3;
+
+    vec3 mixedColor = mix(color1, color2, mixFactor);
+
+    float gradient = vUv.x;
+    vec3 finalColor = mix(vec3(0.0), mixedColor, gradient);
+
+    gl_FragColor = vec4(finalColor, 1.0);
 }
